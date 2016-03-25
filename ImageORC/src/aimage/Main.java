95220a8a5da8f5c51f4bb230b5780051721081f3
Alexis Bayle <alexis.bayle@etu.u-bordeaux.fr>
2016-03-25 10:46:10 +0100

    Plus d'erreur pour les chemins

--- ImageORC/src/aimage/Main.java
+++ ImageORC/src/aimage/Main.java
@@ -9,7 +9,7 @@ public class Main {
         String alexisPath = "/Users/alexis/ImageORC/ImageORC/images";
         String raphaelPath = "/Users/raphael/Documents/DUT-Info/S4/S4_Image/projet/ImageORC/images";
         String pathDefault = alexisPath;
-        if (args[0].equals("1")){
+        if (args.length > 0 && args[0].equals("1")){
             pathDefault = raphaelPath;
         }
 
